78a6d8e05aebcbcf01c276b77895082a2df578be
PratikGarai <pratikgarai0208@gmail.com>
2022-03-19 16:44:37 +0530

    tutorialspoint scraper done

--- tutorialspoint/main.py
+++ tutorialspoint/main.py
@@ -7,6 +7,7 @@ import os
 
 BASE_URL = "https://www.tutorialspoint.com"
 MAIN_URL = "https://www.tutorialspoint.com/python/index.htm"
+HEADINGS = {"h1", "h2", "h3", "h4", "h5", "h6"}
 
 
 def process_class_name(st: str) -> str:
@@ -31,11 +32,11 @@ def scrape_page(url: str) -> Tuple[int, Union[BeautifulSoup, None]]:
         raise Exception(f"Error in scraping page {url} : {e}")
 
 
-def get_content(page : BeautifulSoup, name : str) -> str :
-    try : 
+def get_content(page: BeautifulSoup, name: str) -> str:
+    try:
         main_article = page.select_one("article.content div.text")
         return main_article.text
-    except Exception as e : 
+    except Exception as e:
         raise Exception(f"Error in getting main content of {str} : {e}")
 
 
@@ -43,7 +44,7 @@ def save_as_csv(
     data, fname: str, index: Optional[bool] = False, cnames: Optional[str] = None
 ) -> None:
     try:
-        if not os.path.isdir("save_data") :
+        if not os.path.isdir("save_data"):
             os.mkdir("save_data")
         df = pd.DataFrame(data)
         if cnames:
@@ -63,16 +64,41 @@ def scrape_tutorialspoint() -> None:
         assert status == 200, f"Status {status}"
 
         links = page.select("ul.toc.chapters a")
-        for i in links : 
-            try :
-                print(i.text)
-                print(BASE_URL+i.get("href"))
-            except : 
+        for link in links:
+            try:
+                sub_text = link.text.split(" ")[2]
+                sub_url = BASE_URL + link.attrs["href"]
+                sub_status, sub_page = scrape_page(sub_url)
+                if sub_status != 200:
+                    f += 1
+                    print(
+                        f"Failure ---- Problem scraping sub page {sub_text}. Status : {sub_status}"
+                    )
+                    continue
+
+                paras = sub_page.select(
+                    ".tutorial-content > :not(div)"
+                )
+                heading = "Intro"
+                content = ""
+                for i in paras:
+                    if i.name not in HEADINGS:
+                        content += i.text + "\n"
+                    else:
+                        data.append((sub_text, sub_url, heading, content))
+                        content = ""
+                        heading = i.text
+            except:
                 f += 1
-                print(f"Failure ---- Problem in getting content of sub page {sub_text}. Error : {e}")
+                print(
+                    f"Failure ---- Problem in getting content of sub page {sub_text}. Error : {e}"
+                )
                 continue
-        
-        # save_as_csv(data, "tutorials_data.csv", cnames=["section", "url", "data"])
+            p += 1
+            print(f"Success ---- Scraped page {sub_text}")
+            time.sleep(0.5)  # avoid ddos ban
+
+        save_as_csv(data, "tutorials_data.csv", cnames=["section", "url", "heading", "data"])
         print("\n===================================")
         print(f"Total : {p+f}\tPass : {p}\tFail : {f}")
     except Exception as e:
